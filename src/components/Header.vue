6e3f6113dc5f9f06e71d703900b2f41e08b0dfd8
FrancoCaneti <CAnetifranco95@hmail.com>
2021-05-10 16:10:29 +0200

    flags

---
+++ src/components/Header.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="container"></div>
+</template>
+
+<script>
+export default {
+  name: "Header",
+};
+</script>
+
+<style>
+.container {
+  height: 60px;
+  width: 100%;
+  background-color: crimson;
+}
+</style>
