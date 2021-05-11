0eb1d6a07347a2470dca893a30a19e70ad4bfde8
FrancoCaneti <CAnetifranco95@hmail.com>
2021-05-11 17:41:52 +0200

    bacheca

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,14 +1,56 @@
 <template>
   <div class="container">
     <img src="../assets/netflics.png" alt="" />
+    <input type="text" id="" name="" v-model.trim="ricerca" />
+    <button class="" @click.prevent="$emit('ricerca', risultati)"></button>
   </div>
 </template>
 
 <script>
+import axios from "axios";
 export default {
   name: "Header",
+  data() {
+    return {
+      ricerca: "",
+      risultati: [],
+    };
+  },
+  updated() {
+    this.getApi();
+    this.getApitv();
+  },
+  methods: {
+    getApi() {
+      axios
+        .get("https://developers.themoviedb.org/3/search/movie", {
+          params: {
+            apikey: "9df4bf10e65cc42712e864ea9c9db428",
+            query: this.ricerca,
+          },
+        })
+        .then((res) => {
+          this.risultati = res.data;
+        });
+    },
+    getApitv() {
+      axios
+        .get("https://developers.themoviedb.org/3/search/tv", {
+          params: {
+            apikey: "9df4bf10e65cc42712e864ea9c9db428",
+            query: this.ricerca,
+          },
+        })
+        .then((res) => {
+          this.risultati = res.data;
+        });
+    },
+  },
 };
 </script>
+
+
+
 
 <style>
 .container {
